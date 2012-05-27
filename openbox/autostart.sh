6a24ee37771ec3e25a22c653f855c1cf47d8813c
Urukrama <ukdasa@yahoo.com>
2012-05-27 11:27:59 +0100

    Openbox and Pekwm config 27 May 2012

---
+++ openbox/autostart.sh
@@ -0,0 +1,11 @@
+#!/bin/sh 
+# feh stores the last background in .fehbg 
+#eval `cat $HOME/.fehbg` &
+thunar --daemon &
+docker &
+sleep 2 && lal &
+#sleep 5 && bbpager -w &
+#gtodo &
+#/home/urukrama/.config/osdsh/osdsh_script &
+#pypanel &
+#xautolock -locker  "xte 'key Scroll_Lock'" -corners 0+00 -cornerdelay 0 &
