cd16a62be6e07b2bdd4d615edf81b669d7426e9e
Urukrama <ukdasa@yahoo.com>
2012-07-22 20:00:09 +0100

    autostart vertical dock

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -1,10 +1,13 @@
 #!/bin/sh 
 # feh stores the last background in .fehbg 
 #eval `cat $HOME/.fehbg` &
+/bin/sh  $HOME/.fehbg &
 thunar --daemon &
-docker &
-sleep 2 && lal &
-#sleep 5 && bbpager -w &
+docker -vertical &
+#sleep 2 && lal &
+sleep 5 && bbpager -w &
+bbdock -d 24x24 &
+sleep 2 && tdc &
 #gtodo &
 #/home/urukrama/.config/osdsh/osdsh_script &
 #pypanel &
