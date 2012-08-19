5e9c1d3d79a68254ef540bf451986c8adb391696
Urukrama <ukdasa@yahoo.com>
2012-08-19 11:09:42 +0100

    ibus-m17n layouts

--- scripts/Dmenu_raise.sh
+++ scripts/Dmenu_raise.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Use dmenu to launch or raise and focus running applications
 
-CMD=$(dmenu_path | dmenu -b -fn '-*-nu-*-*-*-*-*-*-*-*-*-*-*-*' -nb '#545043' -nf '#D2C7A7' -sb '#D2C7A7' -sf '#545043')
+CMD=$(dmenu_path | dmenu -i -b -fn '-*-nu-*-*-*-*-*-*-*-*-*-*-*-*' -nb '#545043' -nf '#D2C7A7' -sb '#D2C7A7' -sf '#545043')
 
 # if no instance of the app has been started, launch one now
 if [ -z "`wmctrl -lx | grep -i $CMD`" ]; then
@@ -12,5 +12,3 @@ else
     wmctrl -i -R $app_on_any_desk
     #wmctrl -i -a $app_on_any_desk
 fi;
-
-
